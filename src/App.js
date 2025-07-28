1e89fec1b909c84c86220cd03c4139bff9175f5a
rchellee <ruerasritchelle07@gmail.com>
2025-07-28 18:07:06 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,101 @@
-import './App.css';
+import "./App.css";
+import {
+  FaSearch,
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaPinterest,
+} from "react-icons/fa";
+import logo from "./assets/logo.png";
+import girl from "./assets/me.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is my Portfolio</h1>
+      <header>
+        <nav>
+          <div className="navbar">
+            <div className="logo">
+              <a href="#">Portfolio.</a>
+            </div>
+            <ul className="menu">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/latest">Latest</a>
+              </li>
+              <li>
+                <a href="/category">Category</a>
+              </li>
+              <li>
+                <a href="/services">Services</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+            <div className="search-box">
+              <input type="text" placeholder="Search here..." />
+              <a href="#">
+                <FaSearch />
+              </a>
+            </div>
+          </div>
+        </nav>
+        <div className="content">
+          <div className="text-content">
+            <div className="text">Hello, It's Me </div>
+            <div className="name">Ritchelle Rueras</div>
+            <div className="job">
+              <div className="job">
+                <span>And I'm a</span>
+                <div className="typing-text">
+                  <span className="one">Web </span>
+                  <span className="two">Developer</span>
+                </div>
+              </div>
+            </div>
+            <div className="buttons">
+              <button>About Me</button>
+              <button>Follow Me</button>
+            </div>
+          </div>
+          <div className="girl">
+            <img src={girl} alt="Hannah Lennon" />
+          </div>
+        </div>
+        <div className="media-icons">
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebookF />
+          </a>
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedinIn />
+          </a>
+          <a
+            href="https://pinterest.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaPinterest />
+          </a>
+        </div>
+      </header>
     </div>
   );
 }
